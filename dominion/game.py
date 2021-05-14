341a8a30086165a07d18ffb59ad233c24a2dda24
michael <michaelbutterfield1996@gmail.com>
2021-05-14 17:55:19 -0500

    Updated game print method.

--- dominion/game.py
+++ dominion/game.py
@@ -14,13 +14,21 @@ class Game:
     def _platcol(self, cards):
         return "Prosperity" == [card["Set Name"] for card in cards][random.randrange(0, len(cards))]
     
+    def _card_to_str(self, card):
+        return f'{card["Set Name"] + ":" :<11} {card["Card Name"]}'
+
+    def _deck_stats(self):
+        remaining_cards = len(self.load_pickle())
+        total_cards = len(self.reload_csv())
+        return f'Remaining Cards: {remaining_cards}\nTotal Cards: {total_cards}'
+
     def __str__(self):
         cards_print = ""
         for card in self.cards:
-            cards_print = cards_print + f"{card}\n"
-        return f"{cards_print}\nShelters: {self.has_shelters}\nPlatinums and Colonies: {self.has_platcol}"
+            cards_print = cards_print + f"{self._card_to_str(card)}\n"
+        return f"{cards_print}\nShelters: {self.has_shelters}\nPlatinums and Colonies: {self.has_platcol}\n\n{self._deck_stats()}"
     
-    def pick(self, num_cards = 10):
+    def pick(self, num_cards = 10): 
         available = self.load_pickle()
         total_cards = len(available)
         if total_cards < num_cards:
@@ -30,13 +38,13 @@ class Game:
                 indx = reload.index(card)
                 reload[indx], reload[len(reload)-count] = reload[len(reload)-count], reload[indx]
                 count = count + 1
-            available = self.rearrange(reload[:-total_cards], num_cards - total_cards) + reload[-total_cards:]
+            available = self.shuffle(reload[:-total_cards], num_cards - total_cards) + reload[-total_cards:]
         else:
-            available = self.rearrange(available, num_cards)
+            available = self.shuffle(available, num_cards)
         self.to_pickle(available[:-num_cards])
         return available[-num_cards:]
 
-    def rearrange(self, cards, picks = 10):
+    def shuffle(self, cards, picks = 10):
         total_cards = len(cards)
         for i in range(picks):
             rng = random.randrange(0, total_cards-i)
@@ -51,7 +59,7 @@ class Game:
             return self.reload_csv()
 
     def reload_csv(self):
-        df = pd.read_csv("../data/Owned Cards.csv")
+        df = pd.read_csv('../data/Owned Cards.csv')
         return df.to_dict("records")
 
     def to_pickle(self, cards):
